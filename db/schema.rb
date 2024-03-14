8125ed50980f313301b6159bdfdc60180f9b3aae
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2024-03-13 23:16:10 -0300

    feat: Curso Ruby on Rails 6 MVC - Listando Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_13_015027) do
+ActiveRecord::Schema.define(version: 2024_03_13_223205) do
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
