6937380bb63846c0a547836d3eebb2b89e486358
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2024-03-30 20:06:52 -0300

    feat: 8.Curso Ruby on Rails 6 MVC - Excluindo um Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  #before action para carregar todas as ações de todos os metodos 
+  before_action :set_article, only: [:show,:edit,:update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
   end
 
   def new
@@ -22,23 +24,33 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
       render :edit
     end
   end
+  
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
 
+    redirect_to root_path,status: :see_other
+  end
 
   private
 
   def article_params
     params.require(:article).permit(:title,:body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id]) 
   end
 
 end
